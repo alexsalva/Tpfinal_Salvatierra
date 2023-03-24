459f27eeccbb66a5527502950b8439eeb2cff592
alexsalva <106403600+alexsalva@users.noreply.github.com>
2023-03-23 22:45:39 -0300

    educa

--- Tpfinal_Salvatierra/src/app/app.module.ts
+++ Tpfinal_Salvatierra/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SysComponent } from './components/sys/sys.component';
 
 
 @NgModule({
@@ -21,12 +23,15 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
+    EducacionComponent,
+    SysComponent,
 
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Tpfinal_Salvatierra/src/app/components/sys/sys.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sys',
+  templateUrl: './sys.component.html',
+  styleUrls: ['./sys.component.css']
+})
+export class SysComponent {
+
+}
